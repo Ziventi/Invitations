b372cb73202103c61d4cc5cddc5a1a0812aa1189
Zavid <zavidegbue@gmail.com>
2021-08-24 23:44:11 +0100

    Make start on table interface

---
+++ code/app/utils/constants.ts
@@ -0,0 +1,16 @@
+export const NUMBER_OF_TABLES = 12;
+export const GUESTS_PER_TABLE = 10;
+export const TABLE_NAMES = [
+  'Dragon',
+  'Psychic',
+  'Water',
+  'Ice',
+  'Grass',
+  'Electric',
+  'Fire',
+  'Ground',
+  'Rock',
+  'Steel',
+  'Poison',
+  'Flying'
+];
