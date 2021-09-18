2135d09335047c97a55772c66242cf55e426a108
Zavid <zavidegbue@gmail.com>
2021-09-18 20:40:46 +0100

    Supporting add SVGs to PDF

--- code/cli/controller/commands/generate.ts
+++ code/cli/controller/commands/generate.ts
@@ -1,9 +1,13 @@
 import * as dotenv from 'dotenv';
 import ejs from 'ejs';
 import { ExifTool } from 'exiftool-vendored';
+import express from 'express';
 import fs from 'fs-extra';
 import sass from 'node-sass';
 import puppeteer, { Browser } from 'puppeteer';
+
+import { Server } from 'http';
+import path from 'path';
 
 import { Guest } from '../utils/classes';
 import * as Utils from '../utils/functions';
@@ -11,10 +15,32 @@ import * as Paths from '../utils/paths';
 
 dotenv.config();
 
+const app = express();
+const {
+  NUMBER_BOLA,
+  NUMBER_CHIDERA,
+  NUMBER_DEBORAH,
+  SS_PUBLIC_LISTS_ID,
+  SS_WISHLIST_SHEET_ID
+} = process.env;
+
+app.use(express.static(path.join(__dirname, '../../assets')));
+
+const PUBLIC_LISTS_URL = Utils.getSpreadsheetUrl(SS_PUBLIC_LISTS_ID!);
+const WISHLIST_URL = `${PUBLIC_LISTS_URL}#gid=${SS_WISHLIST_SHEET_ID!}`;
+
+let assetServer: Server;
 let browser: Browser;
 let exiftool: ExifTool;
 
-const TEST_NUMBER = 17;
+const resources: Record<string, unknown> = {};
+
+(async () => {
+  const menu = await import(`${Paths.RESOURCES_DIR}/menu.json`);
+  const notices = await import(`${Paths.RESOURCES_DIR}/notices.json`);
+  resources.menu = menu.default;
+  resources.notices = notices.default;
+})();
 
 /**
  * Generates the invitation files.
@@ -51,19 +77,21 @@ function transpileSass() {
  * @param refreshCache Indicates whether the data cache should be refreshed.
  * @returns A promise fulfilled when all HTML files have been generated.
  */
-async function generateHTMLFiles(refreshCache: boolean): Promise<void[]> {
+async function generateHTMLFiles(refreshCache: boolean): Promise<void> {
   console.info('Generating HTML files...');
+  const TEST_NUMBER = 17;
+
   const guests = await Utils.loadGuestList(refreshCache);
-  const promises = guests
-    .slice(TEST_NUMBER, TEST_NUMBER + 1)
-    .map(createGuestHTML);
-  return Promise.all(promises);
+  const promises = guests.slice(TEST_NUMBER, TEST_NUMBER + 1).map((guest) => {
+    const outputFile = `${Paths.OUTPUT_DIR}/html/${guest.name}.html`;
+    return createHTMLPage(Paths.TEMPLATE_FILE, outputFile, guest);
+  });
+  await Promise.all(promises);
 }
 
 /**
  * Generates the PDF files from each of the guest HTML files.
  * @param refreshCache Indicates whether the data cache should be refreshed.
- * @returns A promise fulfilled when all PDF files have been generated.
  */
 async function generatePDFFiles(): Promise<void> {
   fs.ensureDirSync(`${Paths.OUTPUT_DIR}/pdf`);
@@ -71,45 +99,19 @@ async function generatePDFFiles(): Promise<void> {
 
   browser = await puppeteer.launch();
   exiftool = new ExifTool();
+  assetServer = app.listen(3000);
 
   const filenames = fs.readdirSync(`${Paths.OUTPUT_DIR}/html`);
   const promises = filenames.map((filename) => {
     const [name] = filename.split('.');
     const html = Utils.readFileContent(`${Paths.OUTPUT_DIR}/html/${name}.html`);
-    return createGuestPDFPage(html, name);
+    return createPDFPage(html, name);
   });
 
   await Promise.all(promises);
   await browser.close();
   await exiftool.end();
-}
-
-/**
- * Creates a single HTML file for a guest from the template.
- * @param guest The guest whose information is on the page.
- * @returns A promise fulfilled when the HTML file is created.
- */
-async function createGuestHTML(guest: Guest): Promise<void> {
-  const outputFile = `${Paths.OUTPUT_DIR}/html/${guest.name}.html`;
-  await createHTMLPage(Paths.TEMPLATE_FILE, outputFile, guest);
-}
-
-/**
- * Creates a single PDF file for a guest from the HTML file.
- * @param html The HTML string to be used for generating the PDF.
- * @param name The name of the guest.
- * @returns A promise fulfilled when the PDF file is created.
- */
-async function createGuestPDFPage(html: string, name: string): Promise<void> {
-  const path = `${Paths.OUTPUT_DIR}/pdf/${name}.pdf`;
-  try {
-    await createPDFPage(html, path);
-    await exiftool.write(path, { Title: `Invite to ${name}` }, [
-      '-overwrite_original'
-    ]);
-  } catch (err) {
-    Utils.error(err);
-  }
+  assetServer.close();
 }
 
 /**
@@ -117,52 +119,30 @@ async function createGuestPDFPage(html: string, name: string): Promise<void> {
  * @param templateFile The input template EJS file.
  * @param outputFile The HTML file output path.
  * @param guest The guest whose details will go on the invite.
- * @returns A promise fulfilled when the HTML page is created.
  */
 async function createHTMLPage(
   templateFile: string,
   outputFile: string,
   guest: Guest
 ): Promise<void> {
-  const {
-    NUMBER_BOLA,
-    NUMBER_CHIDERA,
-    NUMBER_DEBORAH,
-    SS_PUBLIC_LISTS_ID,
-    SS_WISHLIST_SHEET_ID
-  } = process.env;
-
-  const publicListsURL = Utils.getSpreadsheetUrl(SS_PUBLIC_LISTS_ID!);
-  const wishListUrl = `${publicListsURL}#gid=${SS_WISHLIST_SHEET_ID!}`;
-
   try {
     const data = await fs.readFile(templateFile, 'utf8');
     const template = ejs.compile(data, { root: Paths.TEMPLATES_DIR });
 
-    const { default: menu } = await import(`${Paths.RESOURCES_DIR}/menu.json`);
-    const { default: notices } = await import(
-      `${Paths.RESOURCES_DIR}/notices.json`
-    );
-
     const html = template({
+      assetsDir: Paths.ASSETS_DIR,
       contacts: {
         Bola: NUMBER_BOLA,
         Chidera: NUMBER_CHIDERA,
         Deborah: NUMBER_DEBORAH
       },
       cssFile: Paths.STYLES_OUTPUT_FILE,
-      images: {
-        signature: `${Paths.ASSETS_DIR}/signature.svg`,
-        waves: `${Paths.ASSETS_DIR}/waves.svg`
-      },
       guest,
-      resources: {
-        menu,
-        notices
-      },
+      hostname: 'http://localhost:3000',
+      resources,
       lists: {
-        guest: publicListsURL,
-        wish: wishListUrl
+        guest: PUBLIC_LISTS_URL,
+        wish: WISHLIST_URL
       }
     });
     await fs.outputFile(outputFile, html);
@@ -172,24 +152,28 @@ async function createHTMLPage(
 }
 
 /**
- * Helper function for creating PDF pages.
- * @param html The HTML content to be used for the page.
- * @param outputPath The path to which the PDF will be saved.
- * @returns A promise fulfilled when the PDF page is created.
+ * Creates a single PDF file for a guest from the HTML file.
+ * @param html The HTML string to be used for generating the PDF.
+ * @param name The name of the guest.
  */
-async function createPDFPage(html: string, outputPath: string) {
+async function createPDFPage(html: string, name: string): Promise<void> {
+  const outputPath = `${Paths.OUTPUT_DIR}/pdf/${name}.pdf`;
   try {
     const page = await browser.newPage();
-    await page.setContent(html);
+    await page.goto(`data:text/html,${encodeURIComponent(html)}`, {
+      waitUntil: 'networkidle0'
+    });
     await page.addStyleTag({ url: Paths.FONTS_URL });
     await page.addStyleTag({ path: Paths.STYLES_OUTPUT_FILE });
-    await page.emulateMediaType('screen');
     await page.evaluateHandle('document.fonts.ready');
     await page.pdf({
       format: 'a4',
       path: outputPath,
       printBackground: true
     });
+    await exiftool.write(outputPath, { Title: `Invite to ${name}` }, [
+      '-overwrite_original'
+    ]);
   } catch (err) {
     Utils.error(err);
   }
