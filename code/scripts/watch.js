2f7f42dcf6d981ee8c395619367e91e76e0679fd
Zavid <zavidegbue@gmail.com>
2021-12-19 15:36:56 +0000

    Fix watch

--- code/scripts/watch.js
+++ code/scripts/watch.js
@@ -4,14 +4,42 @@ const log4js = require('log4js');
 const { spawn } = require('child_process');
 const path = require('path');
 
-const logger = log4js.getLogger();
+log4js.configure({
+  appenders: {
+    Ziventi: {
+      type: 'console',
+      layout: {
+        type: 'pattern',
+        pattern: '%[[%x{ln} %p]%] - %m',
+        tokens: {
+          ln: () => {
+            const pad = (value, length = 2) =>
+              value.toString().padStart(length, '0');
+            const dt = new Date();
+            const hour = pad(dt.getHours());
+            const min = pad(dt.getMinutes());
+            const seconds = pad(dt.getSeconds());
+            const ms = pad(dt.getMilliseconds(), 3);
+            return `${pad(hour)}:${pad(min)}:${pad(seconds)}.${ms}`;
+          }
+        }
+      }
+    }
+  },
+  categories: {
+    default: { appenders: ['Ziventi'], level: 'debug' }
+  }
+});
+const logger = log4js.getLogger('cheese');
 logger.level = 'info';
+
+const ROOT = path.resolve(__dirname, '..');
 
 let rebuildInProgress = false;
 
 chokidar
   .watch(['**/*.ts', '*/**/tsconfig.json'], {
-    cwd: '..',
+    cwd: ROOT,
     ignored: '**/node_modules/**',
     persistent: true,
     awaitWriteFinish: false
@@ -39,7 +67,7 @@ async function rebuildProject(filePath) {
   await new Promise((resolve) => {
     logger.info(`Rebuilding project '${cwd}'...'`);
     const child = spawn('npm', ['run', 'build'], {
-      cwd: path.join(__dirname, cwd)
+      cwd: path.join(ROOT, cwd)
     });
 
     child.stdout.pipe(process.stdout);
