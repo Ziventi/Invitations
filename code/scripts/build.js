1b676e2bb36f22f25af6225c0257449986ea0477
Zavid <zavidegbue@gmail.com>
2021-12-22 01:06:14 +0000

    Validate specified project exists

--- code/scripts/build.js
+++ code/scripts/build.js
@@ -2,15 +2,17 @@ const path = require('path');
 
 const logger = require('./lib/logger');
 const runner = require('./lib/runner');
-const { ensureProjectSpecified } = require('./lib/validator');
+const Validator = require('./lib/validator');
 
 const projectName = process.argv[2];
-ensureProjectSpecified(projectName);
+Validator.ensureProjectSpecified(projectName);
 
 const PROJECT_DIR =
   projectName === 'utils'
     ? path.join(process.cwd(), projectName)
     : path.join(process.cwd(), 'projects', projectName);
+
+Validator.ensureProjectExists(PROJECT_DIR);
 
 const { run, runSync } = runner(PROJECT_DIR);
 
--- code/scripts/lib/validator.js
+++ code/scripts/lib/validator.js
@@ -1,14 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 const logger = require('./logger');
 
 /**
  * Ensures that a project has been specified.
- * @param {string} project The name of the project.
+ * @param {string} project The project name.
  */
-function ensureProjectSpecified(project) {
+exports.ensureProjectSpecified = (project) => {
   if (!project) {
     logger.fatal('No project specified.');
     process.exit(0);
   }
-}
+};
 
-module.exports = { ensureProjectSpecified };
+/**
+ * Ensures that a specified project exists.
+ * @param {string} project The project name.
+ */
+exports.ensureProjectExists = (project) => {
+  if (!fs.existsSync(project)) {
+    logger.fatal(`Project '${path.basename(project)}' does not exist.`);
+    process.exit(0);
+  }
+};
