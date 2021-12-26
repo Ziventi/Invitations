d82620e52c182174925fef0df6942d4695842a43
Zavid <zavidegbue@gmail.com>
2021-12-26 18:08:31 +0000

    Add entry point check

--- code/scripts/cli.js
+++ code/scripts/cli.js
@@ -1,13 +1,12 @@
-const path = require('path');
-
 const runner = require('./lib/runner');
 const Validator = require('./lib/validator');
 
 const [, , projectName, ...args] = process.argv;
 Validator.ensureProjectSpecified(projectName);
 
-const PROJECT_DIR = path.join(process.cwd(), 'projects', projectName);
+const PROJECT_DIR = Validator.getProjectPath(projectName);
 Validator.ensureProjectExists(PROJECT_DIR);
+Validator.ensureEntryPointExists(PROJECT_DIR);
 
 const { run } = runner(PROJECT_DIR);
 
--- code/scripts/lib/validator.js
+++ code/scripts/lib/validator.js
@@ -4,6 +4,21 @@ const path = require('path');
 const logger = require('./logger');
 
 /**
+ * Ensures that the entry point of the project exists.
+ * @param {string} projectPath The path to the project.
+ */
+exports.ensureEntryPointExists = (projectPath) => {
+  const entryPoint = path.resolve(projectPath, './.dist/main.js');
+  if (!fs.existsSync(entryPoint)) {
+    const projectName = path.basename(projectPath);
+    logger.error(
+      `Entry point for project '${projectName}' does not exist. Try rebuilding the project.`
+    );
+    process.exit(0);
+  }
+};
+
+/**
  * Returns the absolute path of a specified project.
  * @param {string} projectName The name of the project.
  * @returns The absolute path of the project.
