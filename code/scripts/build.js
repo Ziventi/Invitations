3ca69437839782f54fe4ef473f5cb307803e2b90
Zavid <zavidegbue@gmail.com>
2022-01-07 00:19:20 +0000

    Ensure utils is always built first

--- code/scripts/build.js
+++ code/scripts/build.js
@@ -7,20 +7,27 @@ const [, , ...projectNames] = process.argv;
 Validator.ensureProjectSpecified(projectNames[0]);
 
 (async () => {
-  const promises = projectNames.map((projectName) => {
-    const PROJECT_DIR = Validator.getProjectPath(projectName);
-    Validator.ensureProjectExists(PROJECT_DIR);
-    const { run, runSilent } = runner(PROJECT_DIR);
+  if (projectNames.includes('utils')) {
+    await queueProjectBuild('utils');
+  }
 
-    logger.info(`Building project '${projectName}'...`);
-    runSilent('rm', ['-rf', '.dist']);
-
-    return Promise.resolve()
-      .then(() => run('tsc', ['--build']))
-      .then(() => {
-        logger.info(`Finished building '${projectName}'.`);
-      });
-  });
-
+  const promises = projectNames
+    .filter((n) => n !== 'utils')
+    .map(queueProjectBuild);
   await Promise.all(promises);
 })();
+
+function queueProjectBuild(projectName) {
+  const PROJECT_DIR = Validator.getProjectPath(projectName);
+  Validator.ensureProjectExists(PROJECT_DIR);
+  const { run, runSilent } = runner(PROJECT_DIR);
+
+  logger.info(`Building project '${projectName}'...`);
+  runSilent('rm', ['-rf', '.dist']);
+
+  return Promise.resolve()
+    .then(() => run('tsc', ['--build']))
+    .then(() => {
+      logger.info(`Finished building '${projectName}'.`);
+    });
+}
