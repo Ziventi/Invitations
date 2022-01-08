00e2410e7a8c01f47d61043c2b4c3f87ae6d2e43
Zavid <zavidegbue@gmail.com>
2022-01-08 22:02:16 +0000

    Add nginx test

--- docker/scripts/deploy.sh
+++ docker/scripts/deploy.sh
@@ -9,6 +9,7 @@ git pull origin deploy
 ## Update nginx.conf
 cp ./docker/nginx.conf /etc/nginx/sites-available/ziventi.co.uk.conf
 ln -s /etc/nginx/sites-available/ziventi.co.uk.conf /etc/nginx/sites-enabled/
+nginx -t
 
 ## Run the docker script
 ./docker/scripts/build-run.sh
