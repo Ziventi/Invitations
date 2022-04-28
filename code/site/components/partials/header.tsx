4128be82f3f14ea7fe8131b6abbb6ede6ae5cafe
Zavid <zavidegbue@gmail.com>
2022-04-29 00:46:58 +0100

    Make site logo return to homepage

--- code/site/components/partials/header.tsx
+++ code/site/components/partials/header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { ReactElement } from 'react';
 
 import ZiventiLogo from 'public/ziventi-logo.png';
@@ -6,15 +7,17 @@ import ZiventiLogo from 'public/ziventi-logo.png';
 export default function Header(): ReactElement {
   return (
     <header>
-      <Image
-        src={ZiventiLogo}
-        alt={'Ziventi Logo'}
-        priority={true}
-        layout={'fill'}
-        objectFit={'contain'}
-        objectPosition={'left'}
-        className={'site-logo'}
-      />
+      <Link href={'/'}>
+        <Image
+          src={ZiventiLogo}
+          alt={'Ziventi Logo'}
+          priority={true}
+          layout={'fill'}
+          objectFit={'contain'}
+          objectPosition={'left'}
+          className={'site-logo'}
+        />
+      </Link>
     </header>
   );
 }
